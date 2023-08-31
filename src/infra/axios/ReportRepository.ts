35de97b6ca1bf22c67d15a9ca62a8952955dec9e
cristhoffer-santos <cristhoffer.santos@jbq.global>
2023-08-31 11:11:28 -0300

    refactor: change console.log to logger

--- src/infra/axios/ReportRepository.ts
+++ src/infra/axios/ReportRepository.ts
@@ -119,7 +119,7 @@ export class ReportRepository implements IReportRepository {
   deleteFiles(): void {
     fs.readdir("files", (err, files) => {
       if (err) {
-        console.error("Erro ao ler a pasta:", err)
+        logger.error("Erro ao ler a pasta:", err)
         return
       }
 
@@ -130,7 +130,7 @@ export class ReportRepository implements IReportRepository {
         // Remove o arquivo
         fs.unlink(filePath, (err) => {
           if (err) {
-            console.error(`Erro ao deletar o arquivo ${file}:`, err)
+            logger.error(`Erro ao deletar o arquivo ${file}:`, err)
           } else {
             logger.info(`Arquivo ${file} foi deletado com sucesso.`)
           }
